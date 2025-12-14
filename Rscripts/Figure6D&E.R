71c8073edd8a28fbe4267db28de6754d5e632026
Christopher Penfold <cap76@cam.ac.uk>
2025-12-14 16:45:54 +0000

    Update Figure6D&E.R

--- Rscripts/Figure6D&E.R
+++ Rscripts/Figure6D&E.R
@@ -9,7 +9,7 @@ library(stringr)
 
 set.seed(1) 
 
-saveext = "./"
+saveext = "./CREST/"
 dir.create(saveext)
 dir.create(paste(saveext,"/Markers/",sep=""))
 dir.create(paste(saveext,"/DimRed/",sep=""))
